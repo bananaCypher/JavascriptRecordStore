6e58ed91b340bc979f7a9921e24c9bf9d2fe5a14
Callum Hogg <bananaCypher@gmail.com>
2016-01-31 13:21:39 +0000

    Added some tests for Record to ensure they have an Artist, Title and Price

--- tests.js
+++ tests.js
@@ -1,2 +1,17 @@
 var assert = require('assert');
 var Record = require('./record');
+
+describe('Record', function(){
+    it('should have an artist', function(){
+        var record = new Record('Pink Floyd', 'Dark Side of the Moon', 2000);
+        assert.equal(record.artist, 'Pink Floyd');
+    });
+    it('should have a title', function(){
+        var record = new Record('Pink Floyd', 'Dark Side of the Moon', 2000);
+        assert.equal(record.title, 'Dark Side of the Moon'); 
+    });
+    it('should have a price', function(){
+        var record = new Record('Pink Floyd', 'Dark Side of the Moon', 2000);
+        assert.equal(record.price, 2000); 
+    });
+});
