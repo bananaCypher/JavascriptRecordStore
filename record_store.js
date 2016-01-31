ec0667c3aaeba270aef276f5fe247f09faff7878
Callum Hogg <bananaCypher@gmail.com>
2016-01-31 13:41:57 +0000

    Got the RecordStore record inventory tset passing

--- record_store.js
+++ record_store.js
@@ -1,6 +1,14 @@
 var RecordStore = function(name, city){
     this.name = name;
     this.city = city;
+    this.inventory = [];
 };
+RecordStore.prototype = {
+    addRecords: function(){
+        for (record of arguments) {
+            this.inventory.push(record);
+        }
+    },
+}
 
 module.exports = RecordStore;
