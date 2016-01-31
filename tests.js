5670504b2f6a10939db8ca7a207e9b159ecfce26
Callum Hogg <bananaCypher@gmail.com>
2016-01-31 13:29:26 +0000

    Added some tests for RecordStore to ensure they have a Name and City

--- tests.js
+++ tests.js
@@ -1,5 +1,6 @@
 var assert = require('assert');
 var Record = require('./record');
+var RecordStore = require('./record_store');
 
 describe('Record', function(){
     it('should have an artist', function(){
@@ -13,5 +14,16 @@ describe('Record', function(){
     it('should have a price', function(){
         var record = new Record('Pink Floyd', 'Dark Side of the Moon', 2000);
         assert.equal(record.price, 2000); 
+    });
+});
+
+describe('Record Store', function(){
+    it('should have a name', function(){
+        var recordStore = new RecordStore('Vinyl Villians', 'Edinburgh');
+        assert.equal(recordStore.name, 'Vinyl Villians');
+    });
+    it('should have a city', function(){
+        var recordStore = new RecordStore('Vinyl Villians', 'Edinburgh');
+        assert.equal(recordStore.city, 'Edinburgh');
     });
 });
