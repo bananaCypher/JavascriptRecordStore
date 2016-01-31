4352a2f93edb0775f666b06ff5986934b17ff263
Callum Hogg <bananaCypher@gmail.com>
2016-01-31 13:44:29 +0000

    Got the RecordStore balance test passing

--- record_store.js
+++ record_store.js
@@ -1,7 +1,8 @@
-var RecordStore = function(name, city){
+var RecordStore = function(name, city, balance){
     this.name = name;
     this.city = city;
     this.inventory = [];
+    this.balance = balance;
 };
 RecordStore.prototype = {
     addRecords: function(){
